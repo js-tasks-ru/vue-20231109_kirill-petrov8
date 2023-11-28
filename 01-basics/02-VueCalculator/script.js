358ea4da4b1ae24ab8858f6c0dc3be3f0584d52b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-11-28 20:01:21 +0300

    Added solution for task: VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,41 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const calculatorOperators = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+createApp({
+  data() {
+    return {
+      operands: [0, 0],
+      operator: 'sum',
+      operations: [
+        {
+          operator: 'sum',
+          text: '➕',
+        },
+        {
+          operator: 'subtract',
+          text: '➖',
+        },
+        {
+          operator: 'multiply',
+          text: '✖',
+        },
+        {
+          operator: 'divide',
+          text: '➗',
+        },
+      ],
+    };
+  },
+
+  computed: {
+    result() {
+      return calculatorOperators[this.operator]?.(...this.operands);
+    },
+  },
+}).mount('#app');
